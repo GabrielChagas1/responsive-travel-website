692771fd7b13bd393455ac8bea32f9594039c4c1
Gabriel Chagas <gabrielserq@outlook.com>
2022-01-16 00:28:43 -0300

    chore: js scroll reveal origin top.

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,22 +115,22 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
-// const sr = ScrollReveal({
-//     distance: '60px',
-//     duration: 2800,
-//     // reset: true,
-// })
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2800,
+    // reset: true,
+})
 
 
-// sr.reveal(`.home__data, .home__social-link, .home__info,
-//            .discover__container,
-//            .experience__data, .experience__overlay,
-//            .place__card,
-//            .sponsor__content,
-//            .footer__data, .footer__rights`,{
-//     origin: 'top',
-//     interval: 100,
-// })
+sr.reveal(`.home__data, .home__social-link, .home__info,
+           .discover__container,
+           .experience__data, .experience__overlay,
+           .place__card,
+           .sponsor__content,
+           .footer__data, .footer__rights`,{
+    origin: 'top',
+    interval: 100,
+})
 
 // sr.reveal(`.about__data, 
 //            .video__description,
